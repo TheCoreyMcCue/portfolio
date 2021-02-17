fe1582dbc1dee4dfdf14d41c5566b39ab3c8a360
Corey McCue <coreysmccue@gmail.com>
2021-02-17 06:58:11 +0100

    implement edit and update functions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: %i[show edit update destroy]
   def index
     @portfolio_items = Portfolio.all
   end
@@ -22,12 +23,30 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    set_portfolio
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+        format.json { render :edit, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @portfolios.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
-
-
 
 private
 
 def portfolio_params
-    params.require(:portfolio).permit(:title, :body, :subtitle)
+  params.require(:portfolio).permit(:title, :body, :subtitle)
+end
+
+def set_portfolio
+  @portfolio_item = Portfolio.find(params[:id])
 end
